8b281834d6e6cfef268bf83e477f1dda54d90d90
emusute1212 <miyanishi1212@gmail.com>
2022-05-04 16:39:31 +0900

    feat: Repositoryをtestableコードへ

--- lib/data/repository/keep_item_repository.dart
+++ lib/data/repository/keep_item_repository.dart
@@ -5,7 +5,9 @@ import 'package:mykeep/utils/stock_items_ext.dart';
 import '../db/my_keep_database.dart';
 
 class KeepItemRepository {
-  final database = MyKeepDatabase();
+  final MyKeepDatabase database;
+
+  KeepItemRepository(this.database);
 
   Stream<List<KeepItem>> observeKeepItems() {
     return database
@@ -13,10 +15,11 @@ class KeepItemRepository {
         .map((it) => it.map((e) => e.toKeepItem()).toList());
   }
 
-  Future<void> addKeepItem(String title, String? imageUrl, String targetUrl) async {
+  Future<void> addKeepItem(
+      String title, String? imageUrl, String targetUrl) async {
     StockItemsCompanion targetItem = StockItemsCompanion(
-      title: Value(title),
-      imageUrl: Value(imageUrl),
+        title: Value(title),
+        imageUrl: Value(imageUrl),
       targetUrl: Value(targetUrl),
       createAt: Value(DateTime.now())
     );
