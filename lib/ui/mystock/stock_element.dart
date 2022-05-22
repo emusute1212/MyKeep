ad5a2edbf9065949775f7e3dc8bd739186c719dc
emusute1212 <miyanishi1212@gmail.com>
2022-05-22 16:14:23 +0900

    feat: チェックボタンを実装

--- lib/ui/mystock/stock_element.dart
+++ lib/ui/mystock/stock_element.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mykeep/data/entity/keep_item.dart';
 
+// TODO デカクラスになっているのでコンポーネントを分ける
 class StockElement extends HookConsumerWidget {
   const StockElement({
     Key? key,
@@ -17,54 +18,81 @@ class StockElement extends HookConsumerWidget {
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: InkWell(
-        onTap: () => onTapItem(item),
-        child: Column(
-          children: <Widget>[
-            Ink.image(
-              height: 120,
-              fit: BoxFit.fitHeight,
-              image: NetworkImage(
-                item.imageUrl ?? "",
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.only(
-                top: 8,
-                left: 10,
-                right: 10,
-                bottom: 8,
-              ),
-              child: Text(
-                item.title,
-                style: const TextStyle(
-                  fontFamily: 'SFProDisplay',
-                  color: Color(0xFF000000),
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+      child: Stack(
+        children: [
+          InkWell(
+            onTap: () => onTapItem(item),
+            child: Column(
+              children: <Widget>[
+                Ink.image(
+                  height: 120,
+                  fit: BoxFit.fitHeight,
+                  image: NetworkImage(
+                    item.imageUrl ?? "",
+                  ),
                 ),
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.only(
-                top: 2,
-                left: 10,
-                right: 10,
-              ),
-              child: Text(
-                item.createAt.toString(),
-                style: const TextStyle(
-                  fontFamily: 'SFProDisplay',
-                  color: Color(0xFF898989),
-                  fontSize: 11,
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(
+                    top: 8,
+                    left: 10,
+                    right: 10,
+                    bottom: 8,
+                  ),
+                  child: Text(
+                    item.title,
+                    style: const TextStyle(
+                      fontFamily: 'SFProDisplay',
+                      color: Color(0xFF000000),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(
+                    top: 2,
+                    left: 10,
+                    right: 10,
+                  ),
+                  child: Text(
+                    item.createAt.toString(),
+                    style: const TextStyle(
+                      fontFamily: 'SFProDisplay',
+                      color: Color(0xFF898989),
+                      fontSize: 11,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              width: 28,
+              height: 28,
+              child: ElevatedButton(
+                child: const Icon(
+                  Icons.check,
+                  color: Colors.white,
+                  size: 18,
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: const Color(0x809A9A9A),
+                  onPrimary: Colors.black,
+                  elevation: 0,
+                  padding: const EdgeInsets.all(0),
+                  shape: const CircleBorder(),
+                ),
+                onPressed: () {},
               ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
