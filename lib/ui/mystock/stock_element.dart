e5455811a43ecfd48cd56245824a205163f4e614
emusute1212 <miyanishi1212@gmail.com>
2024-03-02 22:53:50 +0900

    feat: impl animation

--- lib/ui/mystock/stock_element.dart
+++ lib/ui/mystock/stock_element.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
@@ -22,89 +23,122 @@ class StockElement extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      elevation: 0,
-      margin: EdgeInsets.zero,
-      child: Stack(
-        children: [
-          InkWell(
-            onTap: () => onTapItem(item),
-            child: Column(
-              children: <Widget>[
-                Ink.image(
-                  height: 120,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                  image: getImageProviderFromUrl(
-                    item.imageUrl,
-                    (err) {
-                      print("getImageProviderFromUrl error: $err");
-                    },
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.only(
-                    top: 8,
-                    left: 10,
-                    right: 10,
-                  ),
-                  child: Text(
-                    item.title,
-                    strutStyle: const StrutStyle(
-                      fontSize: 14,
-                      height: 1.4,
-                    ),
-                    style: const TextStyle(
-                      color: Color(0xFF000000),
-                      fontWeight: FontWeight.w700,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.only(
-                    top: 2,
-                    left: 10,
-                    right: 10,
-                    bottom: 8,
-                  ),
-                  child: Text(
-                    _displayDateFormatter.format(item.createAt),
-                    style: const TextStyle(
-                      color: Color(0xFF898989),
-                      fontSize: 11,
-                      fontWeight: FontWeight.w400,
+    // Change color animation
+    final AnimationController changeColorAnimationController =
+        useAnimationController(
+      duration: const Duration(milliseconds: 200),
+    );
+    final changeColorAnimation = ColorTween(
+      begin: const Color(0x802D2D2D),
+      end: const Color(0xFF09BC8A),
+    ).animate(changeColorAnimationController);
+    useAnimation(changeColorAnimationController);
+
+    // Fadeout animation
+    final AnimationController fadeOutAnimationController =
+        useAnimationController(
+      duration: const Duration(milliseconds: 200),
+    );
+    final fadeOutAnimation = Tween<double>(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(fadeOutAnimationController);
+    useAnimation(fadeOutAnimationController);
+
+    return Opacity(
+      opacity: fadeOutAnimation.value,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        elevation: 0,
+        margin: EdgeInsets.zero,
+        child: Stack(
+          children: [
+            InkWell(
+              onTap: () => onTapItem(item),
+              child: Column(
+                children: <Widget>[
+                  Ink.image(
+                    height: 120,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                    image: getImageProviderFromUrl(
+                      item.imageUrl,
+                      (err) {
+                        print("getImageProviderFromUrl error: $err");
+                      },
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: Container(
-              margin: const EdgeInsets.all(8),
-              width: 28,
-              height: 28,
-              decoration: const ShapeDecoration(
-                color: Color(0x802D2D2D),
-                shape: CircleBorder(),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(
+                      top: 8,
+                      left: 10,
+                      right: 10,
+                    ),
+                    child: Text(
+                      item.title,
+                      strutStyle: const StrutStyle(
+                        fontSize: 14,
+                        height: 1.4,
+                      ),
+                      style: const TextStyle(
+                        color: Color(0xFF000000),
+                        fontWeight: FontWeight.w700,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(
+                      top: 2,
+                      left: 10,
+                      right: 10,
+                      bottom: 8,
+                    ),
+                    child: Text(
+                      _displayDateFormatter.format(item.createAt),
+                      style: const TextStyle(
+                        color: Color(0xFF898989),
+                        fontSize: 11,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              child: IconButton(
-                splashRadius: 18,
-                padding: EdgeInsets.zero,
-                icon: const Icon(Icons.check_rounded),
-                color: Colors.white,
-                iconSize: 18,
-                onPressed: () => onTapItemCheck(item),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                width: 28,
+                height: 28,
+                decoration: ShapeDecoration(
+                  color: changeColorAnimation.value,
+                  shape: const CircleBorder(),
+                ),
+                child: IconButton(
+                  splashRadius: 18,
+                  padding: EdgeInsets.zero,
+                  icon: const Icon(Icons.check_rounded),
+                  color: Colors.white,
+                  iconSize: 18,
+                  onPressed: () {
+                    changeColorAnimationController.forward().then((value) {
+                      fadeOutAnimationController.forward().then((value) {
+                        onTapItemCheck(item);
+                        changeColorAnimationController.reset();
+                        fadeOutAnimationController.reset();
+                      });
+                    });
+                  },
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
