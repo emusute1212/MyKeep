b9363dd36ba49b9e348f68b84a945bbbb5040a7e
emusute1212 <miyanishi1212@gmail.com>
2024-03-05 22:42:03 +0900

    feat: Update libs
    - svg
    - ogp

--- lib/data/repository/ogp_repository.dart
+++ lib/data/repository/ogp_repository.dart
@@ -1,12 +1,12 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:metadata_fetch/metadata_fetch.dart';
 import 'package:mykeep/data/entity/ogp_entity.dart';
+import 'package:ogp_data_extract/ogp_data_extract.dart';
 
 final ogpRepositoryProvider = Provider((ref) => OgpRepository());
 
 class OgpRepository {
   Future<OgpEntity> loadOgpEntity(String url) async {
-    final data = await MetadataFetch.extract(url);
+    final data = await OgpDataExtract.execute(url);
     if (data == null) {
       return OgpEntity.empty;
     }
